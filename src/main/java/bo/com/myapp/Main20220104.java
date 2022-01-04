8708c35b8d555f59e7e81a73d84641cc8acb8917
bocastle <bocastle1213@gmail.com>
2022-01-04 22:57:02 +0900

    toDay_algorithm

---
+++ src/main/java/bo/com/myapp/Main20220104.java
@@ -0,0 +1,19 @@
+package bo.com.myapp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main20220104 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+        
+		while( N > 0 ) {
+			System.out.println(N);
+			N--;
+		}	
+	}
+}
