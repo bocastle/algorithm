8e4c4698c4b22b74cdfe82d6d8b51eb0eb2a5d4b
bocastle <bocastle1213@gmail.com>
2022-01-10 17:35:01 +0900

    today_algorithm

---
+++ src/main/java/bo/com/myapp/Main20220110.java
@@ -0,0 +1,23 @@
+package bo.com.myapp;
+
+import java.util.Scanner;
+ 
+public class Main20220110 {
+ 
+	public static void main(String args[]){
+		
+		Scanner in=new Scanner(System.in);
+				
+		while(true){
+		
+			int A=in.nextInt();
+			int B=in.nextInt();
+		
+			if(A==0 && B==0){
+				in.close();
+				break;
+			}
+			System.out.println(A+B);
+		}
+	}
+}
