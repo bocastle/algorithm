287c09aec722b83c1ad5f446cacf1d75c95b7538
bocastle <bocastle1213@gmail.com>
2022-01-23 23:15:01 +0900

    today_algorithm

---
+++ src/main/java/bo/com/myapp/Main20220123.java
@@ -0,0 +1,16 @@
+package bo.com.myapp;
+
+import java.util.Scanner;
+import java.util.HashSet;
+ 
+public class Main20220123 {
+ 
+	public static void main(String[] args) {
+		 
+		Scanner in = new Scanner(System.in);
+ 
+		int ch = in.next().charAt(0);
+        
+		System.out.print(ch);
+	}
+}
