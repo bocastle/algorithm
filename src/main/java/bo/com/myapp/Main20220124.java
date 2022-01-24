fb9847c1d2339afa760f3f1031792610d8cbf4ce
bocastle <bocastle1213@gmail.com>
2022-01-24 23:15:02 +0900

    today_algorithm

---
+++ src/main/java/bo/com/myapp/Main20220124.java
@@ -0,0 +1,22 @@
+package bo.com.myapp;
+
+import java.util.Scanner;
+import java.util.HashSet;
+ 
+public class Main20220124 {
+ 
+	public static void main(String[] args) {		
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		String a = in.next();
+		in.close();
+		
+		int sum = 0;
+        
+		for(int i = 0; i < N; i++) {
+			sum += a.charAt(i)-'0';
+		}
+		System.out.print(sum);
+	}
+}
